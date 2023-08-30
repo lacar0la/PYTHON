f1e3f5aa5a85524ef6ae613fcfdb772b5db3ec38
Carolina Hernández S <108106351+lacar0la@users.noreply.github.com>
2023-08-30 19:07:57 +0200

    Create check_web_address.py

---
+++ Regular expresions/check_web_address.py
@@ -0,0 +1,13 @@
+
+"""The check_web_address function checks if the text passed qualifies as a top-level web address, meaning that it contains alphanumeric characters (which includes letters, numbers, and underscores), as well as periods, dashes, and a plus sign, followed by a period and a character-only top-level domain such as ".com", ".info", ".edu", etc."""
+import re
+def check_web_address(text):
+  pattern = r"^\S+\.[a-zA-Z]+$"
+  result = re.search(pattern, text)
+  return result != None
+
+print(check_web_address("gmail.com")) # True
+print(check_web_address("www@google")) # False
+print(check_web_address("www.Coursera.org")) # True
+print(check_web_address("web-address.com/homepage")) # False
+print(check_web_address("My_Favorite-Blog.US")) # True
