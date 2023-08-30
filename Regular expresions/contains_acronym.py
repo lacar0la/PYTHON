a29f499c62c34e1666db09b8beb1b43ca81c6ed6
Carolina Hernández S <108106351+lacar0la@users.noreply.github.com>
2023-08-30 19:07:02 +0200

    Update and rename check_time.py to contains_acronym.py

--- Regular expresions/contains_acronym.py
+++ Regular expresions/contains_acronym.py
@@ -1,7 +1,7 @@
 """The contains_acronym function checks the text for the presence of 2 of more characters or digits surrounded by parentheses, with at least the first character in uppercase(if it’s a letter), returning True if the condition is met, or False otherwise. For example, “Instant messanging (IM) is a set of communication technologies used for text-based communication “should return True since (IM) satisfies the match conditions” Fill in the regular expression in this function """
 
 import re
-def check_time(text):
+def check_tim(text):
   pattern = r"\([A-Z1-9][a-zA-Z1-9]*\)"
   result = re.search(pattern, text)
   return result != None
