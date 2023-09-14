4f24ab350c6b867abc7f63e0b85dbeb08ce7d15e
Carolina Hernández S <108106351+lacar0la@users.noreply.github.com>
2023-09-14 18:34:53 +0200

    Update new_directory.py

--- basics/new_directory.py
+++ basics/new_directory.py
@@ -1,5 +1,6 @@
 import os
-
+#function that creates a new directory inside the current working direcotry, then creates a new empty file inside the new directory
+#and returns the list of the files in that directory.
 def new_directory(directory, filename):
   # Before creating a new directory, check to see if it already exists
   if os.path.isdir(directory) == False:
@@ -10,6 +11,9 @@ def new_directory(directory, filename):
   with open (filename, 'w+') as file:
     file.close()
     pass
+  os.chdir("..")
 
   # Return the list of files in the new directory
   return (os.listdir(directory))
+
+print(new_directory("PythonPrograms", "script.py"))
