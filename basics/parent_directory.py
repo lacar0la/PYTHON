2824772132d249cc2e3b712b9b11d7f60f1d33f7
Carolina Hernández S <108106351+lacar0la@users.noreply.github.com>
2023-09-14 18:22:55 +0200

    Create parent_directory.py

---
+++ basics/parent_directory.py
@@ -0,0 +1,12 @@
+#This function returns the name of the directory that's located just above the current working directory
+
+import os
+def parent_directory():
+  # Create a relative path to the parent 
+  # of the current working directory 
+  relative_parent = os.path.join(os.getcwd(), os.pardir)
+
+  # Return the absolute path of the parent directory
+  return os.path.abspath(relative_parent)
+
+print(parent_directory())
